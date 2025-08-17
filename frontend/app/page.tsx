c7ca3ab26fe4f8cc13f49b0dbb7cd0edd495a5a5
Evan Law <17935481+evanlaw-dev@users.noreply.github.com>
2025-08-17 12:34:02 -0400

    test

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -55,8 +55,7 @@ function HomeContent() {
           `[Zoom Apps] meetingID=${meetingCtx?.meetingID} | meetingTopic=${meetingCtx?.meetingTopic}`
         );
         console.log(
-          `[Zoom Apps] userID=${userCtx?.screenName} | userName=${userCtx?.role}`
-        );
+          `[Zoom Apps] user screenName=${userCtx?.screenName} | participantId=${userCtx?.participantUUID} | role=${userCtx?.role} | status=${userCtx?.status}`        );
 
       } catch (e) {
         // Not running inside Zoom or SDK not available; keep silent in production
