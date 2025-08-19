8a6d7aab7eab2e57864e66e25e7b5a5596006dab
Evan Law <17935481+evanlaw-dev@users.noreply.github.com>
2025-08-19 10:09:19 -0400

    update constant

--- backend/config/constants.js
+++ backend/config/constants.js
@@ -1,3 +1,3 @@
 module.exports = {
-  MEETING_ID: process.env.MEETING_ID || 'a8f52a02-5aa8-45ec-9549-79ad2a194fa4',
+  MEETING_ID: process.env.MEETING_ID || '64ca6b89-8df7-458f-a2f1-bcd4e41b1614',
 };
